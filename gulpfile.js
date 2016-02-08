ec6b07e92d3a58b7a22ccde81ce34a543e0aaea1
Aidan Boyle <aidan@boylecreations.com>
2016-02-07 21:04:14 -0500

    gulpfule update with node-bourbon libraries

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,56 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint     = require('gulp-jshint');
 var sass       = require('gulp-sass');
+var refills    = require('node-refills').includePaths;
 var concat     = require('gulp-concat');
+var jshint     = require('gulp-jshint');
 var uglify     = require('gulp-uglify');
 var rename     = require('gulp-rename');
 var livereload = require('gulp-livereload');
+//var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+        scss: 'public/stylesheets/style.scss'
+};
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('javascripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('public/scss/*.scss')
-        .pipe(sass())
+    return gulp.src(paths.scss)
+        .pipe(sass({
+          // includePaths: require('node-neat').with('other/path', 'another/path')
+          // - or -
+          includePaths: ['sass'].concat(refills)
+          //require('node-neat').includePaths
+        }))
+        //.pipe(sourcemaps.init())  // Process the original sources
+        //.pipe(sass())
+        //.pipe(sourcemaps.write()) // Add the map to modified source.
+        //.pipe(sass())
         .pipe(gulp.dest('public/stylesheets'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('public/javascripts/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('public/javascripts'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('public/javascripts/*.js', ['lint', 'scripts']);
-    gulp.watch('public/scss/*.scss', ['sass']);
+    gulp.watch('public/stylesheets/*.scss', ['sass']);
 });
 
 // Default Task
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    args: {
+        welcome: {
+          title: 'Boyle Creations',
+          desc: 'A cool website',
+          waypoint: '0vh'
+        },
+        blog: {
+          title: 'Check out my Blog',
+          desc: 'a bunch of blog info',
+          waypoint: '100vh'
+        },
+        portfolio: {
+          title: 'My Projects',
+          desc: 'Handpicked projects from over the  years',
+          waypoint: '200vh'
+        },
+        contact: {
+          title: 'Lets get in Touch',
+          desc: 'I am just an email away',
+          waypoint: '300vh'
+        }
+      }
+  });
 });
 
 module.exports = router;
